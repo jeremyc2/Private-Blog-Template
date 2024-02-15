840cbe38ab25449a6461b0c7890ee474b3239ee6
Jeremy Ryan Chandler <goldenmeso@gmail.com>
2024-02-15 14:56:01 -0600

    Update readme and add suggestions

--- README.md
+++ README.md
@@ -3,21 +3,3 @@
 ```bash
 bunx private-blog-template
 ```
-
-Replace `GOOGLE_CLIENT_ID` and `GOOGLE_CLIENT_SECRET` in `.env` with values from your Google Account
-
-[Setup Google OAuth 2.0](https://console.developers.google.com/apis/credentials)
-
-Update users list in `src/users.json`
-
-### Authorized JavaScript origins
-
-- http://localhost:4321
-
-### Authorized redirect URIs
-
-- http://localhost:4321/api/auth/callback/google
-
-### Suggestions
-
-- [Awesome.md](awesome.md)
--- index.ts
+++ index.ts
@@ -252,6 +252,40 @@ pkg.scripts = { ...pkg.scripts, deploy: "vercel --prod" };
 await Bun.write(`${name}/package.json`, JSON.stringify(pkg, null, "\t"));
 
 /**
+ * README & Docs
+ */
+
+await Bun.write(
+  `${name}/README.md`,
+  `# ${name}
+
+## Setup
+
+Replace \`GOOGLE_CLIENT_ID\` and \`GOOGLE_CLIENT_SECRET\` in [.env](.env) with values from your Google Account
+
+[Setup Google OAuth 2.0](https://console.developers.google.com/apis/credentials)
+
+Update users list in [users.json](src/users.json)
+
+### Authorized JavaScript origins
+
+- http://localhost:4321
+
+### Authorized redirect URIs
+
+- http://localhost:4321/api/auth/callback/google
+
+### Suggestions
+
+- [Awesome.md](awesome.md)`
+);
+
+await Bun.write(
+  `${name}/awesome.md`,
+  await new Response(Bun.file(`${import.meta.dir}/awesome.md`)).text()
+);
+
+/**
  * Git
  */
 
